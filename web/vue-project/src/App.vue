cd2e6344efac0e8381f0253d26ea350f5a4379f7
Farmabarna <a16miqbargim@inspedralbes.cat>
2024-02-28 09:42:26 +0100

    calculadora

--- web/vue-project/src/App.vue
+++ web/vue-project/src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="keypad">
+    <button v-for="number in numbers" :key="number" @click="handleClick(number)">
+      {{ number }}
+    </button>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const numbers = ref([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+const handleClick = (number) => {
+  alert(`Has clicat el número ${number}`);
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.keypad {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.keypad button {
+  width: 2rem;
+  height: 2rem;
+  font-size: 1.5rem;
 }
 </style>
